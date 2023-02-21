73ff392be05f094acb16f5db13db664a281802b1
Nour Aurélien Boulahcen <boulahcn@tcd.ie>
2023-02-21 18:02:13 +0000

    Update App/HoloWay/ci/test.sh

--- App/HoloWay/ci/test.sh
+++ App/HoloWay/ci/test.sh
@@ -8,15 +8,15 @@ CODE_COVERAGE_PACKAGE="com.unity.testtools.codecoverage"
 PACKAGE_MANIFEST_PATH="Packages/manifest.json"
 
 ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' unity-editor} \
-  -projectPath $UNITY_DIR \
+  -projectPath App/HoloWay \
   -runTests \
   -testPlatform $TEST_PLATFORM \
-  -testResults $UNITY_DIR/$TEST_PLATFORM-results.xml \
+  -testResults App/HoloWay/$TEST_PLATFORM-results.xml \
   -logFile /dev/stdout \
   -batchmode \
   -nographics \
   -enableCodeCoverage \
-  -coverageResultsPath $UNITY_DIR/$TEST_PLATFORM-coverage \
+  -coverageResultsPath App/HoloWay/$TEST_PLATFORM-coverage \
   -coverageOptions "generateAdditionalMetrics;generateHtmlReport;generateHtmlReportHistory;generateBadgeReport;" \
   -debugCodeOptimization
 
@@ -28,7 +28,7 @@ elif [ $UNITY_EXIT_CODE -eq 2 ]; then
   echo "Run succeeded, some tests failed";
   if [ $TESTING_TYPE == 'JUNIT' ]; then
     echo "Converting results to JUNit for analysis";
-    saxonb-xslt -s $UNITY_DIR/$TEST_PLATFORM-results.xml -xsl $CI_PROJECT_DIR/ci/nunit-transforms/nunit3-junit.xslt >$UNITY_DIR/$TEST_PLATFORM-junit-results.xml
+    saxonb-xslt -s App/HoloWay/$TEST_PLATFORM-results.xml -xsl App/HoloWay/ci/nunit-transforms/nunit3-junit.xslt >App/HoloWay/$TEST_PLATFORM-junit-results.xml
   fi
 elif [ $UNITY_EXIT_CODE -eq 3 ]; then
   echo "Run failure (other failure)";
@@ -43,9 +43,9 @@ else
 fi
 
 if grep $CODE_COVERAGE_PACKAGE $PACKAGE_MANIFEST_PATH; then
-  cat $UNITY_DIR/$TEST_PLATFORM-coverage/Report/Summary.xml | grep Linecoverage
-  mv $UNITY_DIR/$TEST_PLATFORM-coverage/$CI_PROJECT_NAME-opencov/*Mode/TestCoverageResults_*.xml $UNITY_DIR/$TEST_PLATFORM-coverage/coverage.xml
-  rm -r $UNITY_DIR/$TEST_PLATFORM-coverage/$CI_PROJECT_NAME-opencov/
+  cat App/HoloWay/$TEST_PLATFORM-coverage/Report/Summary.xml | grep Linecoverage
+  mv App/HoloWay/$TEST_PLATFORM-coverage/$CI_PROJECT_NAME-opencov/*Mode/TestCoverageResults_*.xml App/HoloWay/$TEST_PLATFORM-coverage/coverage.xml
+  rm -r App/HoloWay/$TEST_PLATFORM-coverage/$CI_PROJECT_NAME-opencov/
 else
   {
     echo -e "\033[33mCode Coverage package not found in $PACKAGE_MANIFEST_PATH. Please install the package \"Code Coverage\" through Unity's Package Manager to enable coverage reports.\033[0m"
