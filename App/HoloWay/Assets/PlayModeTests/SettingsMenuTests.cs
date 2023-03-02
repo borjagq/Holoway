fc2e40d8b922bf177a47833e1244d776e3a310a3
Siddharth Shenoy <shenoys@tcd.ie>
2023-03-02 17:09:27 +0000

    Login screen tests

---
+++ App/HoloWay/Assets/PlayModeTests/SettingsMenuTests.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class SettingsMenuTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Debug.Log("Loading settings menu tests...");
+        SceneManager.LoadScene(4);
+    }
+    // Check if the objects are loading properly...
+    [UnityTest]
+    public IEnumerator Test_CheckCamera()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("Main Camera"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckCanvas()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckSettingsMenuPanel()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas/SettingsMenu"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckSettingsMenuPanelHeaderBackground()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas/SettingsMenu/SettingsMenuBackground/SettingsMenuHeaderBackground"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckSettingsMenuPanelHeaderText()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas/SettingsMenu/SettingsMenuBackground/SettingsMenuHeader_Text"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckSettingsMenuPanelBackground()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas/SettingsMenu/SettingsMenuBackground/Icon_Settings"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckSettingsMenuCheckButtons()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas/SettingsMenu/Buttons"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckSettingsMenuPanelButtonsControlsButton()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas/SettingsMenu/Buttons/Button_Controls"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckSettingsMenuPanelButtonsGraphicsButton()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas/SettingsMenu/Buttons/Button_Graphics"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckSettingsMenuPanelButtonsDisplayButton()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas/SettingsMenu/Buttons/Button_Display"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckSettingsMenuPanelButtonsAudioButton()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas/SettingsMenu/Buttons/Button_Audio"));
+    }
+    [UnityTest]
+    public IEnumerator Test_CheckSettingsMenuPanelButtonsBackButton()
+    {
+        yield return null; //Skip the frame
+        Assert.NotNull(GameObject.Find("UICanvas/SettingsMenu/Buttons/Button_Back"));
+    }
+
+    //Check for functionality...
+
+}
--- App/HoloWay/CodeCoverage/Report/Summary.md
+++ App/HoloWay/CodeCoverage/Report/Summary.md
@@ -1,49 +1,53 @@
 ﻿# Summary
 |||
 |:---|:---|
-| Generated on: | 01/03/2023 - 16:45:38 |
+| Generated on: | 02-03-2023 - 16:29:22 |
 | Parser: | MultiReportParser (2x OpenCoverParser) |
 | Assemblies: | 2 |
-| Classes: | 29 |
-| Files: | 29 |
-| Covered lines: | 77 |
-| Uncovered lines: | 687 |
-| Coverable lines: | 764 |
-| Total lines: | 1583 |
-| Line coverage: | 10% (77 of 764) |
+| Classes: | 33 |
+| Files: | 32 |
+| Covered lines: | 24 |
+| Uncovered lines: | 654 |
+| Coverable lines: | 678 |
+| Total lines: | 1466 |
+| Line coverage: | 3.5% (24 of 678) |
 | Covered branches: | 0 |
 | Total branches: | 0 |
-| Covered methods: | 18 |
-| Total methods: | 124 |
-| Method coverage: | 14.5% (18 of 124) |
+| Covered methods: | 7 |
+| Total methods: | 133 |
+| Method coverage: | 5.2% (7 of 133) |
 
 |**Name**|**Covered**|**Uncovered**|**Coverable**|**Total**|**Line coverage**|**Covered**|**Total**|**Branch coverage**|**Covered**|**Total**|**Method coverage**|
 |:---|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|---:|
-|**PlaymodeTests**|**55**|**205**|**260**|**575**|**21.1%**|**0**|**0**|****|**10**|**46**|**21.7%**|
+|**PlaymodeTests**|**11**|**304**|**315**|**670**|**3.4%**|**0**|**0**|****|**2**|**59**|**3.3%**|
 |AvatarRoomTests|0|6|6|19|0%|0|0||0|2|0%|
 |DatabasePlayModeTests|0|37|37|79|0%|0|0||0|4|0%|
 |ExternalAPITests|0|12|12|27|0%|0|0||0|2|0%|
 |LoginScreenTests|0|57|57|204|0%|0|0||0|10|0%|
 |MainMenuTests|0|2|2|13|0%|0|0||0|1|0%|
-|RoomCreationMenuTests|0|91|91|141|0%|0|0||0|17|0%|
-|RoomSelectionMenuTests|55|0|55|92|100%|0|0||10|10|100%|
-|**Scripts**|**22**|**482**|**504**|**1008**|**4.3%**|**0**|**0**|****|**8**|**78**|**10.2%**|
+|RoomCreationMenuTests|0|94|94|144|0%|0|0||0|17|0%|
+|RoomSelectionMenuTests|11|44|55|92|20%|0|0||2|10|20%|
+|SettingsMenuTests|0|52|52|92|0%|0|0||0|13|0%|
+|**Scripts**|**13**|**350**|**363**|**813**|**3.5%**|**0**|**0**|****|**5**|**74**|**6.7%**|
 |APIHelper|0|7|7|19|0%|0|0||0|1|0%|
-|BuildCommand|0|159|159|262|0%|0|0||0|14|0%|
 |CameraTargettingScript|0|32|32|68|0%|0|0||0|4|0%|
+|CommonMenuScript|0|9|9|21|0%|0|0||0|2|0%|
 |DatabaseManager|0|51|51|103|0%|0|0||0|12|0%|
 |FootstepBehaviour|0|10|10|27|0%|0|0||0|2|0%|
 |FootstepBehaviourAnimator|0|9|9|21|0%|0|0||0|2|0%|
 |GetExternalAPIData|0|4|4|16|0%|0|0||0|1|0%|
 |GlobalAudioSettings|0|8|8|18|0%|0|0||0|3|0%|
 |GlobalGameSettings|0|3|3|10|0%|0|0||0|1|0%|
+|GlobalGameStrings|0|6|6|12|0%|0|0||0|2|0%|
 |LoginMenuScript|0|27|27|48|0%|0|0||0|4|0%|
+|LoginResponse|0|2|2|17|0%|0|0||0|4|0%|
 |LoginSceneInitialization|0|30|30|70|0%|0|0||0|1|0%|
 |MainMenuScript|4|35|39|77|10.2%|0|0||2|11|18.1%|
 |PlayerEmoticonHandler|0|20|20|34|0%|0|0||0|4|0%|
 |PlayerMovementScript|0|53|53|99|0%|0|0||0|3|0%|
+|RESPONSE|0|1|1|17|0%|0|0||0|2|0%|
 |RoomCreationMenuScript|0|11|11|28|0%|0|0||0|3|0%|
-|RoomSelectionMenuScript|18|0|18|32|100%|0|0||6|6|100%|
+|RoomSelectionMenuScript|9|9|18|32|50%|0|0||3|6|50%|
 |StopShakingHands|0|3|3|11|0%|0|0||0|1|0%|
 |StopSitToStand|0|3|3|13|0%|0|0||0|1|0%|
 |StopStandToSit|0|4|4|14|0%|0|0||0|1|0%|
