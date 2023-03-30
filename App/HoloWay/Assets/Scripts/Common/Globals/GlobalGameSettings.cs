4cbd180a988e9f4a847b4fd0e6b4b171f8387f3b
Siddharth Shenoy <shenoys@tcd.ie>
2023-03-30 17:20:35 +0100

    Fixed the broken scripts and packages json

--- App/HoloWay/Assets/Scripts/Common/Globals/GlobalGameSettings.cs
+++ App/HoloWay/Assets/Scripts/Common/Globals/GlobalGameSettings.cs
@@ -7,4 +7,13 @@ public class GlobalGameSettings
     public static int CurrentSceneIndex = 0;
     public static int PreviousSceneIndex = 0;
     public static GlobalAudioSettings AudioSettings = new GlobalAudioSettings();
+
+    public static int SCENE_INDEX_LOGIN = 0;
+    public static int SCENE_INDEX_MAINMENU = 1;
+    public static int SCENE_INDEX_ROOMCREATIONMENU = 2;
+    public static int SCENE_INDEX_ROOMSELECTIONMENU = 3;
+    public static int SCENE_INDEX_SETTINGSMENU = 4;
+    public static int SCENE_INDEX_AVATARROOM = 5;
+    public static int SCENE_INDEX_AVATARMODIFICATIONROOM = 6;
+    public static int SCENE_INDEX_WHITEBOARDROOM = 7;
 }
