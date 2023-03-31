137e3e393c5ded164e3c1af8f6ed87717a89e8a6
stefanTogan <63917478+stefanTogan@users.noreply.github.com>
2023-03-31 16:08:09 +0100

    Parse audio file into text with external API

--- App/HoloWay/Assets/Scripts/AudioDectection.cs
+++ App/HoloWay/Assets/Scripts/AudioDectection.cs
@@ -5,11 +5,16 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using System.IO;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public class AudioDectection : MonoBehaviour
 {
     public int sampleWindow = 64;
     private AudioClip microphoneClip;
+    private string id;
 
     // Start is called before the first frame update
     void Start()
@@ -17,40 +22,81 @@ public class AudioDectection : MonoBehaviour
         MicrophoneToAudioClipAsync();
     }
 
+    private static async Task<string> SendFile(HttpClient client, string filePath)
+    {
+        try
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "upload");
+           
+            request.Headers.Add("Transer-Encoding", "chunked");
+            // Path to the audio file
+            var path = @"D:\Facultate\Advance Software\HoloWay-Final\ase-team3-meeting_room\App\HoloWay\Assets\audioscript.mp4";
+            var fileReader = File.OpenRead(path);
+            Debug.Log("file read");
+            var streamContent = new StreamContent(fileReader);
+            request.Content = streamContent;
+            HttpResponseMessage response = await client.SendAsync(request);
+            return await response.Content.ReadAsStringAsync();
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
     public async Task MicrophoneToAudioClipAsync()
     {
         string microphoneName = Microphone.devices[0];
         microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
-
-        SavWav.Save("J:/Unity_Project/ASE_Project/ase-team3-meeting_room/App/HoloWayAssets/audio/myaudio", microphoneClip);
-
+        Debug.Log("I am here I will save the file");
+        var filename = "myaudio";
+        var filepath = Path.Combine(Application.dataPath, filename);
+        // this is the problem
+        SavWav.Save(filepath, microphoneClip);
+        string API_Key = "bdf62e371331482f8e5af4004590de54";
+        
         using (HttpClient httpClient = new HttpClient())
         {
-            httpClient.DefaultRequestHeaders.Add(
-              "Authorization",
-              "bdf62e371331482f8e5af4004590de54"
-            );
+            httpClient.BaseAddress = new Uri("https://api.assemblyai.com/v2/");
+            httpClient.DefaultRequestHeaders.Add("authorization", API_Key);
+            Debug.Log("We save the file");
+            var jsonResult = await SendFile(httpClient, filepath);
+            httpClient.Dispose();
 
-            var json = new
-            {
-                audio_url = "J:/Unity_Project/ASE_Project/ase-team3-meeting_room/App/HoloWayAssets/audio/myaudio.wav"
-            };
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.assemblyai.com/v2/");
+            client.DefaultRequestHeaders.Add("authorization", API_Key);
 
-            StringContent payload = new StringContent(
-              JsonConvert.SerializeObject(json),
-              Encoding.UTF8,
-              "application/json"
-            );
+            var json = new { audio_url = JsonConvert.DeserializeObject<UploadItem>(jsonResult).upload_url };
+            var payload = new StringContent(JsonConvert.SerializeObject(json), Encoding.UTF8, "application/json");
 
-            HttpResponseMessage response = await httpClient.PostAsync(
-              "https://api.assemblyai.com/v2/transcript",
-              payload
-            );
-
+            Debug.Log("Make another call");
+            var response = await client.PostAsync("https://api.assemblyai.com/v2/transcript", payload);
             response.EnsureSuccessStatusCode();
-            var responseJson = await response.Content.ReadAsStringAsync();
-            Debug.Log("hello" +responseJson.ToString());
 
+            var responseJson = await response.Content.ReadAsStringAsync();
+            Debug.Log(responseJson);
+            var responseJson2 = (JObject)JsonConvert.DeserializeObject(responseJson);
+            string id = responseJson2["id"].ToString();
+            if (id != null)
+            {
+                Debug.Log("We have the id");
+                HttpClient client2 = new HttpClient();
+                client2.BaseAddress = new Uri("https://api.assemblyai.com/v2/");
+                client2.DefaultRequestHeaders.Add("Authorization", API_Key);
+                client2.DefaultRequestHeaders.Add("Accepts", "application/json");
+                //"r55nxjfalm-1b41-42c2-acae-ec79c7a53141"
+                var ticketId = "r5975vtnga-95af-4f76-a0f5-5f8daa613c59";
+                var response2 = await client2.GetAsync("https://api.assemblyai.com/v2/transcript/" + ticketId);
+                response2.EnsureSuccessStatusCode();
+                Debug.Log("We tried to retrieve the text");
+                var responseJson3 = await response2.Content.ReadAsStringAsync();
+                Debug.Log(responseJson3);
+                var responseJson4 = (JObject)JsonConvert.DeserializeObject(responseJson3);
+                string text = responseJson4["text"].ToString();
+                Debug.Log("We arrive here");
+                Debug.Log(text);
+            }
         }
     }
     public float GetLoudnessFromMicrophone()
@@ -82,5 +128,10 @@ public class AudioDectection : MonoBehaviour
             totalLoudness += Mathf.Abs(waveData[i]);
         }
         return totalLoudness / sampleWindow;
+    }
+
+    class UploadItem
+    {
+        public string upload_url { get; set; }
     }
 }
