3a6df28453a77410d0760fdd3241b3f06314fe22
Mor3s <nour.boulahcen@gmail.com>
2023-04-06 15:48:13 +0100

    implement Catmul-Rom Whiteboard

---
+++ App/HoloWay/Assets/Scripts/Web/Board/DrawPoints.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawPoints : MonoBehaviour
+{
+    [SerializeField]
+        private GameObject point;
+    [SerializeField]
+        private GameObject interpolatedPointObject;
+    [SerializeField]
+        private GameObject board;
+    [SerializeField]
+        private GameObject Parent;
+    [SerializeField]
+        private Camera cam;
+    [SerializeField]
+        private int stepsPerCurve = 100;
+    List<GameObject> points = new List<GameObject>();
+    List<List<GameObject>> splines = new List<List<GameObject>>();
+    List<GameObject> interpolatedPoints = new List<GameObject>();
+    GameObject crntPointMoved = null;
+    Vector3 crntPointMovedPosition;
+    bool Deletion = false;
+
+    // https://andrewhungblog.wordpress.com/2017/03/03/catmull-rom-splines-in-plain-english/
+    public static List<Vector3> GenerateSpline(List<GameObject> _points, int stepsPerCurve = 3, float tension = 1)
+    {
+        List<Vector3> result = new List<Vector3>();
+ 
+        for (int i = 0; i < _points.Count - 1; i++)
+        {
+            Vector3 prev = i == 0 ? _points[i].transform.position : _points[i - 1].transform.position;
+            Vector3 currStart = _points[i].transform.position;
+            Vector3 currEnd = _points[i + 1].transform.position;
+            Vector3 next = i == _points.Count - 2 ? _points[i + 1].transform.position : _points[i + 2].transform.position;
+ 
+            for (int step = 0; step <= stepsPerCurve; step++)
+            {
+                float t = (float)step / stepsPerCurve;
+                float tSquared = t * t;
+                float tCubed = tSquared * t;
+ 
+                Vector3 interpolatedPoint =
+                    (-.5f * tension * tCubed + tension * tSquared - .5f * tension * t) * prev +
+                    (1 + .5f * tSquared * (tension - 6) + .5f * tCubed * (4 - tension)) * currStart +
+                    (.5f * tCubed * (tension - 4) + .5f * tension * t - (tension - 3) * tSquared) * currEnd +
+                    (-.5f * tension * tSquared + .5f * tension * tCubed) * next;
+ 
+                result.Add(interpolatedPoint);
+            }
+        }
+  
+        return result;
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            CastRay(false);
+        }       
+        if (Input.GetMouseButtonDown(1)) {
+            CastRay(true);
+            DrawSplines();
+        }
+        if (crntPointMoved != null && crntPointMovedPosition != crntPointMoved.transform.position) {
+            DrawSplines();
+            crntPointMovedPosition = crntPointMoved.transform.position;
+        }
+        if (Deletion) {
+            DrawSplines();
+            Deletion = false;
+        }
+        
+    }
+  
+    void DrawSplines() {
+        splines.Add(new List<GameObject>(points));
+            foreach (GameObject _point in interpolatedPoints) {
+                Destroy(_point);
+            }
+            interpolatedPoints.Clear();
+            foreach (List<GameObject> Controlpoints in splines){
+                List<Vector3> spline = GenerateSpline(Controlpoints,stepsPerCurve);
+                Debug.Log(Controlpoints.Count);
+                foreach (Vector3 splinePoints in spline) {
+                    GameObject interpolatedPoint = Instantiate(interpolatedPointObject, new Vector3(splinePoints.x,splinePoints.y,splinePoints.z), Quaternion.identity);
+                    interpolatedPoint.transform.parent = Parent.transform;
+                    interpolatedPoint.transform.Translate(new Vector3(0.01f,0f,0f));
+                    interpolatedPoint.transform.Rotate(0,-90,0);
+                    interpolatedPoints.Add(interpolatedPoint);
+                }
+            }
+            points.Clear();
+    }
+     
+
+
+    void CastRay(bool delete) {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Debug.Log("Ray");
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {   
+            Debug.Log("hit");
+            if (delete){
+                if(hit.collider.name == point.name) {
+                    foreach (List<GameObject> _spline in splines) {
+                        bool deleteSplinePoints = false;
+                        foreach (GameObject _point in _spline) {
+                            if(hit.collider.transform.gameObject.GetInstanceID() == _point.GetInstanceID()) {
+                                deleteSplinePoints = true;
+                                Destroy(_point);
+                                _spline.Remove(_point);
+                                Deletion = true;
+                            }
+                            if (deleteSplinePoints) {
+                                Destroy(_point);
+                                _spline.Remove(_point);
+                            }
+                        }
+                    }
+                    
+                }
+                
+            }else {
+                if(hit.collider.name == board.name) {
+                GameObject _point = Instantiate (point, new Vector3(hit.point.x, hit.point.y,hit.point.z), Quaternion.identity);
+                _point.transform.parent = Parent.transform;
+                _point.transform.Translate(new Vector3(0.03f,0.0f,0.0f));
+                _point.transform.Rotate(0,-90,0);
+                points.Add(_point);
+                }
+                if(hit.collider.name == point.name) {
+                    crntPointMoved = hit.transform.gameObject;
+                    crntPointMovedPosition = hit.transform.position;
+                    Debug.Log(crntPointMoved);
+                }
+            }
+            
+        }
+    }
+
+}
---
+++ App/HoloWay/Assets/Scripts/Web/Board/MovePoint.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePoint : MonoBehaviour
+{
+    
+        private Camera cam;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.FindWithTag("whiteBoard").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnMouseDrag()
+     {
+         float distance_to_screen = cam.WorldToScreenPoint(gameObject.transform.position).z;
+         transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
+      
+     }
+}
