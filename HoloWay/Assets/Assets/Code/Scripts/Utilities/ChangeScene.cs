05ec9151ab8bf0ee83e668f95a8d5538267c8f5a
Mor3s <nour.boulahcen@gmail.com>
2023-04-14 15:25:17 +0100

    Fix Main Menu

--- HoloWay/Assets/Assets/Code/Scripts/Utilities/ChangeScene.cs
+++ HoloWay/Assets/Assets/Code/Scripts/Utilities/ChangeScene.cs
@@ -5,7 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void MoveToScene(int sceneID) {
-        SceneManager.LoadScene(sceneID);
+    private enum Scenes {
+        LoginMenu,
+        MainMenu,
+        SettingsMenu,
+        AvatarModificationMenu,
+        RoomCreationMenu,
+        RoomSelectionMenu,
+        SmallRoom,
+        MediumRoom,
+        LargeRoom,
+        MultiplayerRoom
+    };
+
+    [SerializeField]
+    Scenes scenes = new Scenes();
+    
+    public void MoveToScene() {
+        switch (scenes) {
+            case Scenes.LoginMenu:
+                SceneManager.LoadScene(0);
+                break;
+            case Scenes.MainMenu:
+                SceneManager.LoadScene(1);
+                break;
+            case Scenes.SettingsMenu:
+                SceneManager.LoadScene(2);
+                break;
+            case Scenes.AvatarModificationMenu:
+                SceneManager.LoadScene(3);
+                break;
+            case Scenes.RoomCreationMenu:
+                SceneManager.LoadScene(4);
+                break;
+            case Scenes.RoomSelectionMenu:
+                SceneManager.LoadScene(5);
+                break;
+            case Scenes.SmallRoom:
+                SceneManager.LoadScene(6);
+                break;
+            case Scenes.MediumRoom:
+                SceneManager.LoadScene(7);
+                break;
+            case Scenes.LargeRoom:
+                SceneManager.LoadScene(8);
+                break;
+            case Scenes.MultiplayerRoom:
+                SceneManager.LoadScene(9);
+                break;
+        }
+        
     }
 }
