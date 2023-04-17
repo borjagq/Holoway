1258ee6ef6a4413eab8b3ad6766703d45ea42bab
Siddharth Shenoy <shenoys@tcd.ie>
2023-04-17 15:32:25 +0100

    Updated Network Settings script

--- HoloWay/Assets/Assets/Code/Scripts/Globals/GlobalNetworkSettings.cs
+++ HoloWay/Assets/Assets/Code/Scripts/Globals/GlobalNetworkSettings.cs
@@ -1,12 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
+
+
+public enum NetworkType
+{
+    None = 0,
+    Client = 1,
+    Server = 2,
+    Host = 3
+}
 
 public class GlobalNetworkSettings
 {
     private String IPAddress;
     private int Port;
+
+    private NetworkType NetType;
     public String GetIPAddress()
     {
         return this.IPAddress;
@@ -23,4 +35,34 @@ public class GlobalNetworkSettings
     {
         this.Port = port;
     }
+    public NetworkType GetNetType() { 
+        return this.NetType;
+    }
+    public void SetNetworkType(NetworkType netType)
+    {
+        this.NetType = netType;
+    }
+    public void InitializeNetworkManager(NetworkManager manager)
+    {
+        switch (this.NetType)
+        {
+            case NetworkType.Client:
+                {
+                    //manager.NetworkConfig.NetworkTransport.
+                    manager.StartClient();
+                    break;
+                }
+            case NetworkType.Host:
+                {
+                    manager.StartHost();
+                    break;
+                }
+            case NetworkType.Server:
+                {
+                    manager.StartServer();
+                    break;
+                }
+        }
+    }
+
 }
--- HoloWay/Assets/Assets/Code/Scripts/Menu/JoinRoom.cs
+++ HoloWay/Assets/Assets/Code/Scripts/Menu/JoinRoom.cs
@@ -15,15 +15,19 @@ public class JoinRoom : MonoBehaviour
         string name = roomCodeInput.GetComponent<TMP_InputField>().text;
         switch (name) {
             case "small":
+                GlobalGameSettings.Instance.NetworkSettings.SetNetworkType(NetworkType.Client);
                 SceneManager.LoadScene(6);
                 break;
             case "medium":
+                GlobalGameSettings.Instance.NetworkSettings.SetNetworkType(NetworkType.Client);
                 SceneManager.LoadScene(7);
                 break;
             case "large":
+                GlobalGameSettings.Instance.NetworkSettings.SetNetworkType(NetworkType.Client);
                 SceneManager.LoadScene(8);
                 break;
             case "multiplayer":
+                GlobalGameSettings.Instance.NetworkSettings.SetNetworkType(NetworkType.Client);
                 SceneManager.LoadScene(9);
                 break;
         }
