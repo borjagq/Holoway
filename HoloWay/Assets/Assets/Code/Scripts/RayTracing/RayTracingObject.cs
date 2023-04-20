b948b429a0e2dfb924a8cd514b58e3818bafd10f
Mor3s <nour.boulahcen@gmail.com>
2023-04-20 19:59:19 +0100

    Add Reference to the raytracing

--- HoloWay/Assets/Assets/Code/Scripts/RayTracing/RayTracingObject.cs
+++ HoloWay/Assets/Assets/Code/Scripts/RayTracing/RayTracingObject.cs
@@ -1,6 +1,11 @@
     using UnityEngine;
     [RequireComponent(typeof(MeshRenderer))]
     [RequireComponent(typeof(MeshFilter))]
+
+    // http://blog.three-eyed-games.com/2018/05/03/gpu-ray-tracing-in-unity-part-1/
+    // http://three-eyed-games.com/2018/05/12/gpu-path-tracing-in-unity-part-2/
+    // http://three-eyed-games.com/2019/03/18/gpu-path-tracing-in-unity-part-3/
+    
     public class RayTracingObject : MonoBehaviour
     {
         private void OnEnable()
